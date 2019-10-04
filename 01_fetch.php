eeda6870d358d1462759d7076831a156464c2307
kiang <kiange@gmail.com>
2019-10-04 10:53:54 +0800

    first commit

---
+++ 01_fetch.php
@@ -0,0 +1,125 @@
+<?php
+$cities1 = array(
+    '新北市' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__4/stuA_sch_spckind_all__4_20160530.asp',
+    '臺北市' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__3/stuA_sch_spckind_all__3_20160530.asp',
+    '桃園市' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__5/stuA_sch_spckind_all__5_20160530.asp',
+    '臺中市' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__9/stuA_sch_spckind_all__9_20160530.asp',
+    '臺南市' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__16/stuA_sch_spckind_all__16_20160530.asp',
+    '高雄市' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__18/stuA_sch_spckind_all__18_20160530.asp',
+    '宜蘭縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__1/stuA_sch_spckind_all__1_20160530.asp',
+    '新竹縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__7/stuA_sch_spckind_all__7_20160530.asp',
+    '苗栗縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__8/stuA_sch_spckind_all__8_20160530.asp',
+    '彰化縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__12/stuA_sch_spckind_all__12_20160530.asp',
+    '南投縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__11/stuA_sch_spckind_all__11_20160530.asp',
+    '雲林縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__13/stuA_sch_spckind_all__13_20160530.asp',
+    '嘉義縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__15/stuA_sch_spckind_all__15_20160530.asp',
+    '屏東縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__20/stuA_sch_spckind_all__20_20160530.asp',
+    '臺東縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__21/stuA_sch_spckind_all__21_20160530.asp',
+    '花蓮縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__22/stuA_sch_spckind_all__22_20160530.asp',
+    '澎湖縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__23/stuA_sch_spckind_all__23_20160530.asp',
+    '基隆市' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__2/stuA_sch_spckind_all__2_20160530.asp',
+    '新竹市' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__6/stuA_sch_spckind_all__6_20160530.asp',
+    '嘉義市' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__14/stuA_sch_spckind_all__14_20160530.asp',
+    '金門縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__24/stuA_sch_spckind_all__24_20160530.asp',
+    '連江縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all__25/stuA_sch_spckind_all__25_20160530.asp',
+);
+$cities2 = array(
+    '新北市' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_4/stuA_sch_spckind_all_E_4_20160530.asp',
+    '臺北市' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_3/stuA_sch_spckind_all_E_3_20160530.asp',
+    '桃園市' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_5/stuA_sch_spckind_all_E_5_20160530.asp',
+    '臺中市' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_9/stuA_sch_spckind_all_E_9_20160530.asp',
+    '臺南市' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_16/stuA_sch_spckind_all_E_16_20160530.asp',
+    '高雄市' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_18/stuA_sch_spckind_all_E_18_20160530.asp',
+    '宜蘭縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_1/stuA_sch_spckind_all_E_1_20160530.asp',
+    '新竹縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_7/stuA_sch_spckind_all_E_7_20160530.asp',
+    '苗栗縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_8/stuA_sch_spckind_all_E_8_20160530.asp',
+    '彰化縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_12/stuA_sch_spckind_all_E_12_20160530.asp',
+    '南投縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_11/stuA_sch_spckind_all_E_11_20160530.asp',
+    '雲林縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_13/stuA_sch_spckind_all_E_13_20160530.asp',
+    '嘉義縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_15/stuA_sch_spckind_all_E_15_20160530.asp',
+    '屏東縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_20/stuA_sch_spckind_all_E_20_20160530.asp',
+    '臺東縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_21/stuA_sch_spckind_all_E_21_20160530.asp',
+    '花蓮縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_22/stuA_sch_spckind_all_E_22_20160530.asp',
+    '澎湖縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_23/stuA_sch_spckind_all_E_23_20160530.asp',
+    '基隆市' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_2/stuA_sch_spckind_all_E_2_20160530.asp',
+    '新竹市' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_6/stuA_sch_spckind_all_E_6_20160530.asp',
+    '嘉義市' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_14/stuA_sch_spckind_all_E_14_20160530.asp',
+    '金門縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_24/stuA_sch_spckind_all_E_24_20160530.asp',
+    '連江縣' => 'https://www.set.edu.tw/Stastic_WEB/sta2/doc/stuA_sch_spckind_all_E_25/stuA_sch_spckind_all_E_25_20160530.asp',
+);
+$days = array(
+    '2019/5/28',
+'2019/3/20',
+'2018/10/20',
+'2018/5/28',
+'2018/3/20',
+'2017/10/20',
+'2017/5/31',
+'2017/3/20',
+'2016/10/20',
+'2016/5/30',
+'2016/3/21',
+'2015/10/21',
+'2015/5/28',
+'2015/3/20',
+'2014/10/20',
+'2014/5/28',
+'2014/3/20',
+'2013/10/21',
+'2013/5/28',
+'2013/3/20',
+'2012/10/20',
+'2012/5/28',
+'2012/3/20',
+'2011/10/20',
+'2011/5/30',
+'2011/3/21',
+'2010/10/20',
+'2010/5/28',
+'2010/3/22',
+'2009/10/20',
+'2009/6/1',
+'2009/3/20',
+'2008/10/20',
+);
+foreach($days AS $day) {
+    $theTime = strtotime($day);
+    $theYear = date('Y', $theTime);
+    $theDate = date('Ymd', $theTime);
+    foreach($cities1 AS $city => $url) {
+        $rawPath = __DIR__ . '/raw/' . $city;
+        if(!file_exists($rawPath)) {
+            mkdir($rawPath, 0777, true);
+        }
+        $rawFile = $rawPath . '/' . $theDate . '.html';
+        if(!file_exists($rawFile) || filesize($rawFile) === 0) {
+            if($theYear < 2014) {
+                $url = str_replace('20160530', $theDate, $cities2[$city]);
+            } else {
+                $url = str_replace('20160530', $theDate, $url);
+            }
+            
+            file_put_contents($rawFile, file_get_contents($url));
+        }
+        $c = mb_convert_encoding(file_get_contents($rawFile), 'utf-8', 'big5');
+        $c = str_replace(array(' bgcolor=#eeeeee ', '&nbsp;'), array('', ''), $c);
+        $pos = strrpos($c, '<table');
+        $lines = explode('<tr>', substr($c, $pos));
+        $targetPath = __DIR__ . '/csv/' . $city;
+        if(!file_exists($targetPath)) {
+            mkdir($targetPath, 0777, true);
+        }
+        $fh = fopen($targetPath . '/' . $theDate . '.csv', 'w');
+        $result = array();
+        foreach($lines AS $line) {
+            $parts = preg_split('/\\<td[^\\>]*\\>/', $line);
+            foreach($parts AS $k => $v) {
+                $parts[$k] = trim(strip_tags($v));
+                if(false !== strpos($parts[$k], chr(13))) {
+                    $parts[$k] = explode(chr(13), $parts[$k])[0];
+                }
+            }
+            fputcsv($fh, $parts);
+        }
+    }
+}
